09457be64bb9affb49f6a36b05972ddeb310d6ba
Harlan Kellaway <hkellaway@users.noreply.github.com>
2020-05-02 17:09:17 -0400

    Fix warning in color from hex string

--- FightPandemics/Utilities/UIColorExtension.swift
+++ FightPandemics/Utilities/UIColorExtension.swift
@@ -29,26 +29,21 @@ import UIKit
 
 extension UIColor {
     convenience init(hexString: String, alpha: CGFloat = 1.0) {
-        let hexString: String = hexString.trimmingCharacters(in: CharacterSet.whitespacesAndNewlines)
-        let scanner = Scanner(string: hexString)
+        var hexFormatted: String = hexString.trimmingCharacters(in: CharacterSet.whitespacesAndNewlines).uppercased()
 
-        if hexString.hasPrefix("#") {
-            scanner.scanLocation = 1
+        if hexFormatted.hasPrefix("#") {
+            hexFormatted = String(hexFormatted.dropFirst())
         }
 
-        var color: UInt32 = 0
-        scanner.scanHexInt32(&color)
+        assert(hexFormatted.count == 6, "Invalid hex code used.")
 
-        let mask = 0x000000FF
-        let r = Int(color >> 16) & mask
-        let g = Int(color >> 8) & mask
-        let b = Int(color) & mask
+        var rgbValue: UInt64 = 0
+        Scanner(string: hexFormatted).scanHexInt64(&rgbValue)
 
-        let red   = CGFloat(r) / 255.0
-        let green = CGFloat(g) / 255.0
-        let blue  = CGFloat(b) / 255.0
-
-        self.init(red: red, green: green, blue: blue, alpha: alpha)
+        self.init(red: CGFloat((rgbValue & 0xFF0000) >> 16) / 255.0,
+                  green: CGFloat((rgbValue & 0x00FF00) >> 8) / 255.0,
+                  blue: CGFloat(rgbValue & 0x0000FF) / 255.0,
+                  alpha: alpha)
     }
 
     func toHexString() -> String {
