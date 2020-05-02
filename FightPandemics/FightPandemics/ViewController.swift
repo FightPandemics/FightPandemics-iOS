5182afce50c8562c4ce0c381912b595991926514
Harlan Kellaway <hkellaway@users.noreply.github.com>
2020-05-02 15:02:23 -0400

    Recreate Xcode project

---
+++ FightPandemics/FightPandemics/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FightPandemics
+//
+//  Created by Harlan Kellaway on 5/2/20.
+//  Copyright © 2020 Fight Pandemics. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
