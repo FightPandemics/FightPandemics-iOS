b6187c76ee5325249559f27ab74f12dfe6bcf8e7
ethanswift <ehsonst@gmail.com>
2020-05-11 19:07:04 -0400

    Feed Post UI

---
+++ FightPandemics/Features/Feed/FeedViewController.swift
@@ -0,0 +1,62 @@
+//
+//  FeedViewController.swift
+//  FightPandemics
+//
+//  Created by ehsan sat on 5/11/20.
+//
+//  Copyright (c) 2020 FightPandemics
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in
+//  all copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+//  THE SOFTWARE.
+
+import UIKit
+
+class FeedViewController: UIViewController {
+    var avatar: Avatar!
+    var avatarView: AvatarView!
+//    var postContainer: PostContainer!
+    var feedPost: FeedPost!
+    var postFooter: PostFooter!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+        self.avatar = Avatar(text: "LL")
+        self.avatarView = AvatarView(name: "Lily Luke", location: "Manchester, UK", avatar: avatar)
+        let body1 = "Does anyone have an extra pack of disinfecting Clorox wipes? I would be very grateful if you could sell me one. Two people in our family have chronic pulmonary diseases, including my son. We are isolated, but my husband might infect us, as he works with people a lot."
+        let body2 = "Does anyone have an extra pack of disinfecting Clorox wipes? I would be very grateful if you could sell me one. Two people in our family have chronic pulmonary diseases, including my son. We are isolated, but my husband might infect us, as he works with people a lot."
+        let body = body1 + body2
+        let title = "Disinfecting clorox wipes needed"
+//        self.postContainer = PostContainer(body: body, title: title)
+//        self.postContainer.frame.origin.y = 100
+//        self.view.addSubview(postContainer)
+        self.feedPost = FeedPost(type: "  Medical Supplies .", title: title, body: body)
+        self.postFooter = PostFooter(likeNum: 12, commentNum: 13, sendNum: 14)
+        let firstView = UIView()
+        firstView.frame = CGRect(x: 0, y: 100, width: (UIScreen.main.bounds.width - 47), height: 400)
+        self.view.addSubview(firstView)
+        firstView.addSubview(avatar)
+        firstView.addSubview(feedPost)
+        let secondView = UIView()
+        secondView.frame = CGRect(x: 0, y: 530, width: (UIScreen.main.bounds.width - 47), height: 300)
+        self.view.addSubview(secondView)
+        secondView.addSubview(postFooter)
+//        self.view.addSubview(avatar)
+//        self.view.addSubview(feedPost)
+//        self.view.addSubview(postFooter)
+}
+}
