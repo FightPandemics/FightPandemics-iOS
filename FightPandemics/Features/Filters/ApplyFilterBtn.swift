67c701bf5eb6d3f7e6190f636e7d9ceb58245219
ethanswift <ehsonst@gmail.com>
2020-05-22 02:29:49 +0430

    file created

--- FightPandemics/Features/Filters/ApplyFilterBtn.swift
+++ FightPandemics/Features/Filters/ApplyFilterBtn.swift
@@ -1,5 +1,5 @@
 //
-//  ClearFilterBtn.swift
+//  ApplyFilterBtn.swift
 //  FightPandemics
 //
 //  Created by ehsan sat on 5/22/20.
@@ -24,23 +24,4 @@
 //  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 //  THE SOFTWARE.
 
-import UIKit
-
-class ClearFilterBtn: UIView {
-    private var clearFilterBtn = UIButton()
-    func setUp() {
-        clearFilterBtn.setAttributedTitle(NSAttributedString(string: "Clear Filters", attributes: [NSAttributedString.Key.font: Fonts.poppinsRegular.customFont(size: 15), NSAttributedString.Key.foregroundColor: UIColor.fightPandemicsNeonBlue()]), for: .normal)
-        clearFilterBtn.backgroundColor = UIColor.clear
-        clearFilterBtn.frame.size.height = 45
-        clearFilterBtn.clipsToBounds = true
-        clearFilterBtn.layer.cornerRadius = clearFilterBtn.frame.size.height / 2
-        clearFilterBtn.layer.borderWidth = 1
-        clearFilterBtn.layer.borderColor = UIColor.fightPandemicsNeonBlue().cgColor
-        clearFilterBtn.makeSubview(of: self)
-            .width(158)
-            .height(45)
-            .top(to: \.topAnchor, constant: 0)
-            .left(to: \.leftAnchor, constant: 0)
-            .right(to: \.rightAnchor, constant: 0)
-    }
-}
+import Foundation
