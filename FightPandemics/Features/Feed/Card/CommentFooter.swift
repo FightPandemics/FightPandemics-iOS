5b4de464fe243309fb0923ced895a94e6fb06378
ethanswift <ehsonst@gmail.com>
2020-06-11 02:29:08 +0430

    modified

--- FightPandemics/Features/Feed/Card/CommentFooter.swift
+++ FightPandemics/Features/Feed/Card/CommentFooter.swift
@@ -24,4 +24,42 @@
 //  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 //  THE SOFTWARE.
 
-import Foundation
+import UIKit
+
+class CommentFooter: UIView {
+    var timeStamp: String
+    var likeBtn = UIButton()
+    var replyBtn = UIButton()
+    private var timeStampLbl = UILabel()
+    private var stackView = UIStackView()
+    init(timeStamp: String) {
+        self.timeStamp = timeStamp
+        super.init(frame: .zero)
+        setUp()
+    }
+
+    required init?(coder _: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    func setUp() {
+        timeStampLbl.attributedText = NSAttributedString(string: timeStamp, attributes: [NSAttributedString.Key.font: Fonts.worksansRegular.customFont(size: 11), NSAttributedString.Key.foregroundColor: UIColor.fightPandemicsNero()])
+        addSubview(timeStampLbl)
+        likeBtn.setAttributedTitle(NSAttributedString(string: "Like", attributes: [NSAttributedString.Key.font: Fonts.worksansRegular.customFont(size: 11), NSAttributedString.Key.foregroundColor: UIColor.fightPandemicsNero()]), for: .normal)
+        addSubview(likeBtn)
+        replyBtn.setAttributedTitle(NSAttributedString(string: "Reply", attributes: [NSAttributedString.Key.font: Fonts.worksansRegular.customFont(size: 11), NSAttributedString.Key.foregroundColor: UIColor.fightPandemicsNero()]), for: .normal)
+        addSubview(replyBtn)
+        stackView.axis = .horizontal
+        stackView.alignment = .fill
+        stackView.distribution = .fillEqually
+        stackView.spacing = 16
+        stackView.addArrangedSubview(timeStampLbl)
+        stackView.addArrangedSubview(likeBtn)
+        stackView.addArrangedSubview(replyBtn)
+        stackView.makeSubview(of: self)
+            .left(to: \.leftAnchor, constant: 0)
+            .top(to: \.topAnchor, constant: 0)
+            .right(to: \.rightAnchor, constant: 0)
+            .bottom(to: \.bottomAnchor, constant: 0)
+    }
+}
