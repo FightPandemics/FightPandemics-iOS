cc121c7fc14e0537740ad241b64cbe77cf8c26b4
Sara Tavares <29093946+stavares843@users.noreply.github.com>
2020-07-07 18:10:21 +0100

    Update AppDelegate.swift

--- FightPandemics/AppDelegate.swift
+++ FightPandemics/AppDelegate.swift
@@ -26,6 +26,12 @@
 
 import UIKit
 
+
+
+
+
+
+
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_: UIApplication, didFinishLaunchingWithOptions _: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
